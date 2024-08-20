9b663904b16a130b1f9431664e0c7dc53248aeb6
mis7er.n9o <mis7er.n9o@gmail.com>
2024-08-21 00:24:59 +0200

    feat: update bash script db

--- local_db_init.sh
+++ local_db_init.sh
@@ -11,63 +11,39 @@ DB_USER="postgres"
 DB_PASSWORD="funky"
 
 # Set the folder path containing the CSV files
+JSON_USER_FILE="users.csv"
+JSON_USER_FILE_FOLDER="data/"
+JSON_USER_FILE_PATH="$JSON_USER_FILE_FOLDER$JSON_USER_FILE"
 CSV_FOLDER="data/raw"
 CSV_FOLDER_MODIFY="$CSV_FOLDER/tmp"
 
 # Check if the container exists
+echo "=====Docker Container====="
 CONTAINER_EXISTS=$(docker ps -a --filter name="$CONTAINER_NAME" --quiet)
-
+echo ">Container name: $CONTAINER_NAME exist, drop it ..."
 if [ -z "$CONTAINER_EXISTS" ]; then
-    # Container doesn't exist, create a new one
-    echo "PostgreSQL container doesn't exist. Creating a new one..."
-    docker run -d --name "$CONTAINER_NAME" -e POSTGRES_PASSWORD=funky -p 5432:5432 postgres
-    echo "PostgreSQL container created successfully."
-else
-    # Container exists, check if it's running
-    CONTAINER_STATUS=$(docker inspect --format='{{.State.Running}}' "$CONTAINER_NAME")
-
-    if [ "$CONTAINER_STATUS" == "true" ]; then
-        echo "PostgreSQL container is already running."
-    else
-        echo "PostgreSQL container exists but is not running. Starting the container..."
-        docker start "$CONTAINER_NAME"
-        echo "PostgreSQL container started successfully."
-    fi
+    # Stop the container first
+    docker stop "$CONTAINER_NAME"
+    docker rm "$CONTAINER_NAME"
 fi
 
-# Check if the database exists
-DB_EXISTS=$(docker exec "$CONTAINER_NAME" psql -lqt | cut -d \| -f 1 | grep -w "$DB_NAME" | wc -l)
+echo "=====PostgrSQL Container====="
+echo "> Creating PostgreSQL container ..."
+docker run -d --name "$CONTAINER_NAME" -e POSTGRES_PASSWORD=funky -p 5432:5432 postgres
+echo "> ...done"
 
-if [ "$DB_EXISTS" -eq 0 ]; then
-    # Database doesn't exist, create a new one
-    echo "Database '$DB_NAME' doesn't exist. Creating a new one..."
-    docker exec -it "$CONTAINER_NAME" psql -U postgres -c "CREATE DATABASE $DB_NAME;"
-    echo "Database '$DB_NAME' created successfully."
-else
-
-    echo "Database '$DB_NAME' already exists."
-fi
-
+echo "> Creating Database '$DB_NAME' ..."
+docker run --name DB_NAME -e POSTGRES_PASSWORD=DB_PASSWORD -d postgres
+echo "> ...done"
 
 # =====
-# table : DATA
+# table : DATA & USER
 # =====
 
-# Check if the table data exists
-TABLE_DATA_EXISTS=$(docker exec "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -c "
-    SELECT EXISTS (
-        SELECT FROM information_schema.tables
-        WHERE table_name = '$TBL_DATA'
-    );" -tAX)
+echo "=====Create table : $TBL_DATA, $TBL_USER====="
 
-if [ "$TABLE_DATA_EXISTS" = "t" ]; then
-    # Table exists, try to delete it
-    echo "Deleting table '$TBL_DATA'..."
-    docker exec "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -c "DROP TABLE $TBL_DATA;" || true
-fi
-
-
-# Create the table
+# Create the historical data 
+echo "> Creating Table '$TBL_DATA' ..."
 docker exec -i $CONTAINER_NAME psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME <<EOF
 CREATE TABLE IF NOT EXISTS $TBL_DATA (
     asset TEXT,
@@ -81,28 +57,12 @@ CREATE TABLE IF NOT EXISTS $TBL_DATA (
 );
 EOF
 
-echo "Table '$TBL_DATA' created successfully."
+echo "> ...done"
 
-# =====
-# table : USER
-# =====
-
-# Check if the table data exists
-TABLE_USER_EXISTS=$(docker exec "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -c "
-    SELECT EXISTS (
-        SELECT FROM information_schema.tables
-        WHERE table_name = '$TBL_USER'
-    );" -tAX)
-
-if [ "$TABLE_USER_EXISTS" = "t" ]; then
-    # Table exists, try to delete it
-    echo "Deleting table '$TBL_USER'..."
-    docker exec "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" -c "DROP TABLE $TBL_USER;" || true
-fi
-
-# Create the table
+# Create the user table
+echo "> Creating Table '$TBL_USER' ..."
 docker exec -i $CONTAINER_NAME psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME <<EOF
-CREATE TABLE IF NOT EXISTS $TBL_USER (
+CREATE TABLE IF NOT EXISTS "$TBL_USER" (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
@@ -111,20 +71,21 @@ CREATE TABLE IF NOT EXISTS $TBL_USER (
 );
 EOF
 
-echo "Table '$TBL_USER' created successfully."
+echo "> ...done"
+
+
 # =====
 # POPULATE TABLES
 # =====
 
-# OHCVLT DATA
-# ===========
+echo "=====Upload data into table : $TBL_DATA====="
 
 # Check if the folder exists
 if [ ! -d "$CSV_FOLDER" ]; then
-    echo "Error: The specified folder '$CSV_FOLDER' does not exist."
+    echo ">Error: The specified folder '$CSV_FOLDER' does not exist."
     exit 1
 else
-    echo "folder "$CSV_FOLDER" checked"
+    echo ">folder "$CSV_FOLDER" checked"
 fi
 
 # Create a temporary folder to store modified CSV files
@@ -135,7 +96,7 @@ mkdir -p "$CSV_FOLDER_MODIFY"
 
 # Process each CSV file
 for file in $CSV_FOLDER/*.csv; do
-    echo "process file: $file"
+    echo "----- process file: $file"
     # Extract pair from the file name
     pair=$(basename "$file" | cut -d'_' -f1)
     filename=$(basename "$file")
@@ -144,14 +105,14 @@ for file in $CSV_FOLDER/*.csv; do
 done
 
 # Combine all CSV files into one*
-echo "combine all csv from $CSV_FOLDER_MODIFY ..."
+echo ">combine all csv from $CSV_FOLDER_MODIFY ..."
 cat $CSV_FOLDER_MODIFY/*.csv > combined.csv
 # Copy the combined CSV file into the Docker container
-echo "copy combined csv file from $CSV_FOLDER_MODIFY to Container ..."
+echo ">copy combined csv file from $CSV_FOLDER_MODIFY to Container ..."
 docker cp combined.csv $CONTAINER_NAME:/combined.csv
 
-# Access the PostgreSQL container and run the COPY command
-echo "Create table $DB_NAME and bulk import data"
+# Access the PostgreSQL container and copy user data to table
+echo ">Create table $TBL_DATA and bulk import data"
 docker exec -i $CONTAINER_NAME psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME <<EOF
 DO \$\$
 BEGIN
@@ -163,20 +124,30 @@ BEGIN
             END;
 END \$\$;
 EOF
+rm /combined.csv
+echo ">Data OHCVLT import completed."
 
-echo "Data OHCVLT import completed."
 
-
-# USER DATA
-# ===========
+echo "=====Upload data into table : $TBL_USER====="
 
 # Copy the JSON file into the Docker container
-docker cp "$JSON_FILE_PATH" "$CONTAINER_NAME":/tmp/file.json
+docker cp $JSON_USER_FILE_PATH $CONTAINER_NAME:/
+echo "$JSON_USER_FILE_PATH"
+echo "---"
+# Connect to the PostgreSQL container and load the data
+docker exec -i $CONTAINER_NAME psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME <<EOF
+DO \$\$
+BEGIN
+    BEGIN
+        COPY $TBL_USER (id,username,email,hashed_password,role)
+        FROM '/"$JSON_USER_FILE_PATH"' DELIMITER ',' CSV HEADER;
+    EXCEPTION WHEN OTHERS THEN
+        RAISE NOTICE 'Skipping problematic line: %', SQLERRM;
+            END;
+END \$\$;
+EOF
+rm "/$JSON_USER_FILE_PATH"
 
-docker exec -i "$CONTAINER_NAME" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "
-    COPY $TABLE_NAME FROM '/tmp/file.json'
-    WITH (
-        FORMAT 'json',
-        FREEZE 'true'
-    );
-"
+echo "Users import completed."
+
+echo "..... └[°=°]┘ ....  waiting order master !"
