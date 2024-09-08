494a63ad586d0656ed71a8d06b0d80c16fbc7c85
mis7er.n9o <mis7er.n9o@gmail.com>
2024-09-08 22:27:49 +0200

    resolve git issue

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,20 +1,28 @@
+
 -- Connect to  database
-\c ohlcvt
+--\c cryptoDb
 
 -- Create the "historical" table
-CREATE TABLE historical (
+CREATE TABLE ohlc (
     asset TEXT,
-    epoch BIGINT,
-    open NUMERIC,
-    high NUMERIC,
-    low NUMERIC,
-    close NUMERIC,
-    volume NUMERIC,
-    trades NUMERIC
+    dtutc TIMESTAMPTZ NOT NULL,
+    open DOUBLE PRECISION,
+    high DOUBLE PRECISION,
+    low DOUBLE PRECISION,
+    close DOUBLE PRECISION,
+    volume DOUBLE PRECISION,
+    trades DOUBLE PRECISION
 );
 
+CREATE TABLE assets (
+    id SERIAL PRIMARY KEY,
+    asset TEXT NOT NULL,
+    symbol TEXT NOT NULL,
+    exchange TEXT NOT NULL
+); 
+
 -- Create the "user" table
-CREATE TABLE users  (
+CREATE TABLE members  (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
@@ -22,9 +30,13 @@ CREATE TABLE users  (
     role VARCHAR(20) NOT NULL
 );
 
+-- Insert records into the "users" table
+INSERT INTO assets (id, asset, symbol, exchange)
+VALUES
+    (1, 'XXBTZUSD', 'BTZUSD', 'kraken');
 
 -- Insert records into the "users" table
-INSERT INTO users (id, username, email, hashed_password, role)
+INSERT INTO members (id, username, email, hashed_password, role)
 VALUES
     (1, 'john Doe', 'john.doe@example.com', 'password123', 'admin'),
     (2, 'jane Smith', 'jane.smith@example.net', 'password456', 'user');
